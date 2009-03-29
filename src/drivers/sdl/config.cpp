Plombo/fceux

commit 832884a3433bfc68a67acbd00cab6433d2ef8272

SDL: hotkey Del to toggle --mute for avi capturing
git-svn-id: svn://svn.code.sf.net/p/fceultra/code/fceu@1239 cd624aef-3799-4ffa-999e-25a118dd8ffa

src/drivers/sdl/config.cpp
@@ -303,6 +303,7 @@ InitConfig()
     config->addOption(prefix + "IncreaseSpeed", SDLK_EQUALS);
     config->addOption(prefix + "FrameAdvance", SDLK_BACKSLASH);
     config->addOption(prefix + "InputDisplay", SDLK_i);
+    config->addOption(prefix + "MuteCapture", SDLK_DELETE);
     config->addOption(prefix + "Quit", SDLK_ESCAPE);
     //config->addOption(prefix + "Power", 0);
     

src/drivers/sdl/input.cpp
@@ -168,6 +168,7 @@ int movieToggleFrameDisplayKey;
 int lagCounterDisplayKey;
 int SubtitleDisplayKey;
 int InputDisplayKey;
+int MuteCaptureKey;
 
 // this function loads the sdl hotkeys from the config file into the
 // global scope.  this elimates the need for accessing the config file
@@ -205,6 +206,7 @@ void setHotKeys()
 	g_config->getOption("SDL.Hotkeys.MovieToggleFrameDisplay", &movieToggleFrameDisplayKey);
 	g_config->getOption("SDL.Hotkeys.SubtitleDisplay", &SubtitleDisplayKey);
 	g_config->getOption("SDL.Hotkeys.InputDisplay", &InputDisplayKey);
+	g_config->getOption("SDL.Hotkeys.MuteCapture", &MuteCaptureKey);
 	/*
 	config->addOption(prefix + "FrameAdvance", SDLK_BACKSLASH);
 	config->addOption(prefix + "Power", 0);
@@ -415,6 +417,11 @@ KeyboardCommands()
         FCEUI_ToggleInputDisplay();
         extern int input_display;
         g_config->setOption("SDL.InputDisplay", input_display);
+    }
+    
+    if(_keyonly(MuteCaptureKey)) {
+        extern int mutecapture;
+        mutecapture ^= 1;
     }
 
     if(_keyonly(pauseKey)) {

commit 01f54ee06d74b17909828c64f638fc5e5628a737

SDL: fixed compilation error without -DCREATE_AVI introduced in r1239
git-svn-id: svn://svn.code.sf.net/p/fceultra/code/fceu@1245 cd624aef-3799-4ffa-999e-25a118dd8ffa

src/drivers/sdl/config.cpp
@@ -303,7 +303,9 @@ InitConfig()
     config->addOption(prefix + "IncreaseSpeed", SDLK_EQUALS);
     config->addOption(prefix + "FrameAdvance", SDLK_BACKSLASH);
     config->addOption(prefix + "InputDisplay", SDLK_i);
+    #ifdef CREATE_AVI
     config->addOption(prefix + "MuteCapture", SDLK_DELETE);
+    #endif
     config->addOption(prefix + "Quit", SDLK_ESCAPE);
     //config->addOption(prefix + "Power", 0);
     

src/drivers/sdl/input.cpp
@@ -168,7 +168,9 @@ int movieToggleFrameDisplayKey;
 int lagCounterDisplayKey;
 int SubtitleDisplayKey;
 int InputDisplayKey;
+#ifdef CREATE_AVI
 int MuteCaptureKey;
+#endif
 
 // this function loads the sdl hotkeys from the config file into the
 // global scope.  this elimates the need for accessing the config file
@@ -206,7 +208,9 @@ void setHotKeys()
 	g_config->getOption("SDL.Hotkeys.MovieToggleFrameDisplay", &movieToggleFrameDisplayKey);
 	g_config->getOption("SDL.Hotkeys.SubtitleDisplay", &SubtitleDisplayKey);
 	g_config->getOption("SDL.Hotkeys.InputDisplay", &InputDisplayKey);
+	#ifdef CREATE_AVI
 	g_config->getOption("SDL.Hotkeys.MuteCapture", &MuteCaptureKey);
+	#endif
 	/*
 	config->addOption(prefix + "FrameAdvance", SDLK_BACKSLASH);
 	config->addOption(prefix + "Power", 0);
@@ -419,10 +423,12 @@ KeyboardCommands()
         g_config->setOption("SDL.InputDisplay", input_display);
     }
     
+    #ifdef CREATE_AVI
     if(_keyonly(MuteCaptureKey)) {
         extern int mutecapture;
         mutecapture ^= 1;
     }
+    #endif
 
     if(_keyonly(pauseKey)) {
         FCEUI_ToggleEmulationPause();